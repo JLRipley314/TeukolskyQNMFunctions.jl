aca9342966cb18abc2d6844be0e64121a40a3e4a
Justin <lloydripley@gmail.com>
2022-02-09 10:22:57 +0000

    updated Sphere functions to match time evolution code

--- src/Sphere.jl
+++ src/Sphere.jl
@@ -1,189 +1,218 @@
 """
 Spin-weighted spherical harmonics and associated functions and operators.
-We set y ≡ -cosθ
-
-num_l = ny - 2*(max_s+max_m), where max_s and max_m
-are hard coded in Sphere.jl
 """
 module Sphere
 
-include("CustomTypes.jl")
-using .CustomTypes
+setprecision(2048) # BigFloat precision in bits
 
 import FastGaussQuadrature as FGQ
 import Jacobi: jacobi
 
+export Y_vals, cos_vals, sin_vals, swal, swal_laplacian_matrix, swal_filter_matrix, angular_matrix_mult!
+
+function convertBF(v)
+   return parse(BigFloat,"$v")
+end
+
+"""
+number of l angular values given ny theta values
+
+num_l = ny - 2*(max_s+max_m), where max_s and max_m
+are hard coded in Sphere.jl
+
+num_l(
+   ny::Int64
+   )::Int64
+"""
 const max_s = 2
 const max_m = 6
 
-function num_l(ny::myI)::myI
+function num_l(ny::Int64)::Int64
    @assert ny>(2*max_s+2*max_m)+4
    return ny-2*max_s-2*max_m
 end
-"""
-    Y_vals(ny::myI)::Vector{myF}
 
-Computes Gauss-Legendre points (y) over the interval [-1,1].
 """
-function Y_vals(ny::myI)::Vector{myF}
+Computes Gauss-Legendre points (y) over the interval [-1,1].
+
+Y_vals(
+   ny::Int64
+   )::Vector{Float64}
+"""
+function Y_vals(ny::Int64)::Vector{Float64}
 
    roots, weights= FGQ.gausslegendre(ny) 
 
    return roots 
 end
-"""
-    inner_product(
-      v1::Vector{myF},
-      v2::Vector{myF}
-    )::myF
 
+"""
 Computes inner product over the interval [-1,1] using
 Gauss quadrature:
 
 return: ∫ dy v1 conj(v2)
+
+inner_product(
+   v1::Vector{T},
+   v2::Vector{T},
+   )::T where T<:Number
 """
 function inner_product(
-      v1::Vector{myF},
-      v2::Vector{myF}
-   )::myF
+      v1::Array{T,1},
+      v2::Array{T,1}
+   )::T where T<:Number
    ny = size(v1)[1]
 
    roots, weights= FGQ.gausslegendre(ny) 
 
-   s = tomyF(0)
+   s = 0.0
    for j=1:ny
       s += weights[j]*v1[j]*conj(v2[j])
    end
 
    return s 
 end
-"""
-    cos_vals(ny::myI)::Vector{myF}
 
-Computes cos(y) at Gauss-Legendre points over interval [-1,1].
 """
-function cos_vals(ny::myI)::Vector{myF}
+Computes cos(y) at Gauss-Legendre points over interval [-1,1].
+
+cos_vals(
+   ny::Int64
+   )::Vector{Float64}
+"""
+function cos_vals(ny::Int64)::Vector{Float64}
 
    roots, weights= FGQ.gausslegendre(ny) 
 
    return [-pt for pt in roots]
 end
-"""
-    sin_vals(ny::myI)::Vector{myF}
 
-Computes sin(y) at Gauss-Legendre points over interval [-1,1].
 """
-function sin_vals(ny::myI)::Vector{myF}
+Computes sin(y) at Gauss-Legendre points over interval [-1,1].
+
+sin_vals(
+   ny::Int64
+   )::Vector{Float64}
+"""
+function sin_vals(ny::Int64)::Vector{Float64}
 
    roots, weights= FGQ.gausslegendre(ny) 
    
-   return [convert(myF,
-                   sqrt(tomyF(1)-pt)*sqrt(tomyF(1)+pt)
+   return [convert(Float64,
+                   sqrt(1.0-convertBF(pt))*sqrt(1.0+convertBF(pt))
                   )
            for pt in roots
           ]
 end
-"""
-    swal(
-      spin::myI,
-      m_ang::myI,
-      l_ang::myI,
-      y::myF
-    )::myF
 
+"""
 Computes the spin-weighted associated Legendre function Y^s_{lm}(y).
+
+swal(
+   spin::Int64,
+   m_ang::Int64,
+   l_ang::Int64,
+   y::Float64
+   )::Float64
 """
 function swal(
-      spin::myI,
-      m_ang::myI,
-      l_ang::myI,
-      y::myF
-   )::myF
-   
+      spin::Int64,
+      m_ang::Int64,
+      l_ang::Int64,
+      y::Float64
+   )::Float64
+   @assert l_ang>=abs(m_ang)
+
    al = abs(m_ang-spin)
    be = abs(m_ang+spin)
    @assert((al+be)%2==0)
-   n = tomyF(l_ang - ((al+be)/tomyF(2)))
+   n = l_ang - (al+be)/2
 
    if n<0
-      return tomyF(0)
+      return convert(Float64,0)
    end
 
    norm = sqrt(
-      (2*n+al+be+1)*(tomyF(2)^(-al-be-1.0))
-   *  factorial(tomyF(n+al+be))/factorial(tomyF(n+al))
-   *  factorial(tomyF(n      ))/factorial(tomyF(n+be))
+      (2*n+al+be+1)*(convertBF(2)^(-al-be-1.0))
+   *  factorial(convertBF(n+al+be))/factorial(convertBF(n+al))
+   *  factorial(convertBF(n      ))/factorial(convertBF(n+be))
    )
-   norm *= tomyF(-1)^(max(m_ang,-spin))
+   norm *= convertBF(-1)^(max(m_ang,-spin))
 
    return convert(
-      myF,
+      Float64,
       norm
-      *(tomyF(1-y)^(al/2.))
-      *(tomyF(1+y)^(be/2.))
-      *jacobi(tomyF(y),tomyF(n),tomyF(al),tomyF(be))
+      *(convertBF(1-y)^(al/2.))
+      *(convertBF(1+y)^(be/2.))
+      *jacobi(convertBF(y),convertBF(n),convertBF(al),convertBF(be))
    )
 end
-"""
-    swal_vals(
-      ny::myI,
-      spin::myI,
-      m_ang::myI,
-    )::Matrix{myF}
 
+"""
 Computes matrix swal^s_{lm}(y) at Gauss-Legendre points,
 over a grid of l values (and fixed m).
+
+swal_vals(
+   ny::Int64,
+   spin::Int64,
+   m_ang::Int64,
+   )::Matrix{Float64}
 """
 function swal_vals(
-      ny::myI,
-      spin::myI,
-      m_ang::myI,
-   )::Matrix{myF}
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64,
+   )::Matrix{Float64}
 
    roots, weights= FGQ.gausslegendre(ny) 
 
    nl = num_l(ny)
    
-   vals = zeros(myF,ny,nl)
+   vals = zeros(Float64,ny,nl)
+   
+   lmin = max(abs(spin),abs(m_ang))
    
    for k=1:nl
-      l_ang = k-1
+      l_ang = k-1+lmin
       for j=1:length(roots)
          vals[j,k] = swal(spin,m_ang,l_ang,roots[j])
       end
    end
    return vals
 end
-"""
-    swal_laplacian_matrix(
-      ny::myI,
-      spin::myI,
-      m_ang::myI
-    )::Matrix{myF}
 
+"""
 Computes matrix to compute spin-weighted spherical harmonic laplacian.
 To compute the spherical laplacian use the function 
 set_lap!(v_lap,v,lap_matrix)
+
+swal_laplacian_matrix(
+   ny::Int64,
+   spin::Int64,
+   m_ang::Int64
+   )::Matrix{Float64}
 """
 function swal_laplacian_matrix(
-      ny::myI,
-      spin::myI,
-      m_ang::myI
-   )::Matrix{myF}
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64
+   )::Matrix{Float64}
    
    rv, wv = FGQ.gausslegendre(ny) 
 
    nl = num_l(ny) 
 
-   lap = zeros(myF,ny,ny)
+   lap = zeros(Float64,ny,ny)
+
+   lmin = max(abs(spin),abs(m_ang))
 
    for j=1:ny
       for i=1:ny
          for k=1:nl
-            l = k-1
-            lap[j,i] -= (l-spin)*(l+spin+tomyF(1))*(swal(spin,m_ang,l,rv[i])*
-                                                    swal(spin,m_ang,l,rv[j])
-                                                   )
+            l = k-1+lmin
+            lap[j,i] -= (l-spin)*(l+spin+1.0)*(swal(spin,m_ang,l,rv[i])*
+                                               swal(spin,m_ang,l,rv[j])
+                                              )
          end
          lap[j,i] *= wv[j]
       end
@@ -191,35 +220,36 @@ function swal_laplacian_matrix(
 
    return lap
 end
-"""
-    swal_filter_matrix(
-      ny::myI,
-      spin::myI,
-      m_ang::myI
-    )::Matrix{myF}
 
-Computes matrix to compute low pass filter
-of spin-weighted spherical harmonic laplacian.
-To compute the spherical laplacian use the function 
-set_lap!(v_lap,v,lap_matrix)
+"""
+Computes matrix to compute low pass filter.
+
+swal_filter_matrix(
+   ny::Int64,
+   spin::Int64,
+   m_ang::Int64
+   )::Matrix{Float64}
+
+Multiply the matrix on the left: v[i]*M[i,j] -> v[j]
 """
 function swal_filter_matrix(
-      ny::myI,
-      spin::myI,
-      m_ang::myI
-   )::Matrix{myF}
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64
+   )::Matrix{Float64}
    
    rv, wv = FGQ.gausslegendre(ny) 
 
    nl = num_l(ny) 
 
-   filter = zeros(myF,ny,ny)
+   filter = zeros(Float64,ny,ny)
+
+   lmin = max(abs(spin),abs(m_ang))
 
    for j=1:ny
       for i=1:ny
-         for k=1:nl
-            l = k-1
-            filter[j,i] += exp(-tomyF(30)*(l/(nl-tomyF(1)))^10)*(
+         for l=lmin:(nl-1+lmin)
+            filter[j,i] += exp(-30.0*(l/(nl-1.0))^10)*(
                            swal(spin,m_ang,l,rv[i])*
                            swal(spin,m_ang,l,rv[j])
                         )
@@ -230,14 +260,15 @@ function swal_filter_matrix(
 
    return filter
 end
-"""
-    angular_matrix_mult!(
-      f_m,
-      f,
-      mat
-    )
 
+"""
 Matrix multiplication in the angular direction
+
+angular_matrix_mult!(
+   f_m,
+   f,
+   mat
+   )
 """
 function angular_matrix_mult!(
       f_m,
@@ -247,7 +278,7 @@ function angular_matrix_mult!(
    nx, ny = size(f)
    for j=1:ny
       for i=1:nx
-         f_m[i,j] = tomyF(0)
+         f_m[i,j] = 0.0
          for k=1:ny
             f_m[i,j] += f[i,k]*mat[k,j]
          end
--- test/TestSphere.jl
+++ test/TestSphere.jl
@@ -1,27 +1,30 @@
 module TestSphere
 
-export test_swal_inner_product, test_norm_swal_lap
-
-include("../src/CustomTypes.jl")
 include("../src/Sphere.jl")
 include("Norms.jl")
 
-import Test: @test
-
-using .CustomTypes
-import .Norms 
 import .Sphere
+import .Norms 
 
+using Test
 
 const tol = 5e-13
 
+export test_swal_inner_product, test_norm_swal_lap, test_norm_filter
+
 """
-Computes inner product between different Y^s_{lm} functions
+Computes inner product between different Y^s_{lm}
+
+test_swal_inner_product(
+   ny::Int64,
+   spin::Int64,
+   m_ang::Int64
+   )::Nothing
 """
 function test_swal_inner_product(
-      ny::myI,
-      spin::myI,
-      m_ang::myI
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64
    )::Nothing
 
    nl = Sphere.num_l(ny)
@@ -32,7 +35,7 @@ function test_swal_inner_product(
    for i=1:nl
       for j=i:nl
          val = Sphere.inner_product(vals[:,i],vals[:,j])
-         if i==j && i>max(abs(spin),abs(m_ang))
+         if i==j
             @test abs(val-1.0) < tol 
          else
             @test abs(val) < tol 
@@ -46,12 +49,19 @@ end
 Computes the norm of the difference between the numerical and
 exact spin-weighted spherical laplacian for a given
 spin-weighted spherical harmonic.
+
+test_norm_swal_lap(
+   ny::Int64,
+   spin::Int64,
+   m_ang::Int64,
+   l_ang::Int64
+   )::Nothing
 """
 function test_norm_swal_lap(
-      ny::myI,
-      spin::myI,
-      m_ang::myI,
-      l_ang::myI
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64,
+      l_ang::Int64
    )::Nothing
    
    Yv = Sphere.Y_vals(ny)
@@ -59,7 +69,7 @@ function test_norm_swal_lap(
    swal = [Sphere.swal(spin,m_ang,l_ang,y) for y in Yv]
 
    swal_lap_v1 = [-(l_ang-spin)*(l_ang+spin+1.0)*v for v in swal]
-   swal_lap_v2 = zeros(myF,ny) 
+   swal_lap_v2 = zeros(Float64,ny) 
    
    lap = Sphere.swal_laplacian_matrix(ny,spin,m_ang)
    for j=1:ny
@@ -73,6 +83,42 @@ function test_norm_swal_lap(
 
    println("Test: Δ_{S2}Y^s_{lm}:\tny=$ny\tspin=$spin\tm_ang=$m_ang\tl=$l_ang")
    @test abs(n) < tol 
+
+   return nothing
+end
+
+"""
+Test that the swal filter matrix acts as a low pass filter
+for angular perturbations.
+"""
+function test_norm_filter(
+      ny::Int64,
+      spin::Int64,
+      m_ang::Int64,
+      l_ang::Int64
+   )::Nothing
+   
+   Yv = Sphere.Y_vals(ny)
+
+   ## essentially random points multiplied by spherical harmonic;
+   ## with the filter the norm should go to zero
+   v1 = [0.1*(rand()-0.5)*Sphere.swal(spin,m_ang,l_ang,y) for y in Yv]
+   v2 = zeros(Float64,ny) 
+  
+   filter = Sphere.swal_filter_matrix(ny,spin,m_ang)
+
+   for j=1:ny
+      for k=1:ny
+         v2[j] += v1[k]*filter[k,j]
+      end
+   end
+
+   ## compute integral over interval [-1,1] of difference
+   i1 = abs(Sphere.inner_product(ones(ny), abs.(v1)))
+   i2 = abs(Sphere.inner_product(ones(ny), abs.(v2)))
+
+   println("Test: filter(rand) smoothens:\tny=$ny\tspin=$spin\tm_ang=$m_ang\tl_ang=$l_ang")
+   @test i2 < i1 
 
    return nothing
 end
